f8bd9f93ed9a1f0d1aa5ba6f2ac3ecc6b3fa31d2
pran saha <iampransaha@gmail.com>
2021-08-05 18:40:18 +0530

    Add project files.

---
+++ PasswordComplexityChecker/PasswordCheck.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordComplexityChecker
+{
+    class PasswordCheck
+    {
+        public static bool CheckPassword(string password)
+        {
+            if (CheckPasswordForLetters(password)
+                && CheckPasswordForNumbers(password)
+                && CheckPasswordForSymbols(password)
+                && CheckPasswordLength(password)
+                && CheckPasswordForCapitalLetters(password))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool CheckPasswordForLetters(string password)
+        {
+            for (int index = 0; index < password.Length; index++)
+            {
+                if (Char.IsLetter(password[index]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        private static bool CheckPasswordForNumbers(string password)
+        {
+            for (int index = 0; index < password.Length; index++)
+            {
+                if (Char.IsDigit(password[index]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private static bool CheckPasswordForSymbols(string password)
+        {
+            for (int index = 0; index < password.Length; index++)
+            {
+                if (!Char.IsLetterOrDigit(password[index]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private static bool CheckPasswordLength(string password)
+        {
+            if (password.Length >= 6)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private static bool CheckPasswordForCapitalLetters(string password)
+        {
+            for (int index = 0; index < password.Length; index++)
+            {
+                if (Char.IsUpper(password[index]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ PasswordComplexityChecker/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordComplexityChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Password:");
+            string password = Console.ReadLine();
+            if (PasswordCheck.CheckPassword(password))
+            {
+                Console.WriteLine("Password is strong");
+            }
+            else
+            {
+                Console.WriteLine("Password is weak");
+            }
+            Console.ReadLine();
+        }
+    }
+}
